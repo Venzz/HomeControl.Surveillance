22bece473db9da03aa5d8fb1d8e6c75165a0a0ba
Venzz <venexzz@gmail.com>
2017-12-21 21:46:46 +0300

    Added memory caching to file storage implementation to avoid often access to disk.

--- HomeControl.Surveillance.Server/Source/Data/Storage/File/FileStorageService.cs
+++ HomeControl.Surveillance.Server/Source/Data/Storage/File/FileStorageService.cs
@@ -7,20 +7,42 @@ namespace HomeControl.Surveillance.Server.Data.File
 {
     public class FileStorageService: IStorageService
     {
+        private const Int32 CachedFileStreamLength = 128 * 1024 * 1024;
+
         private Stream ActiveFileStream;
         private String ActiveFileName;
-        private Task DataSendingSequence = Task.CompletedTask;
+        private MemoryStream CachedFileStream = new MemoryStream(CachedFileStreamLength);
+        private Task DataFlushingSequence = Task.CompletedTask;
 
         public event TypedEventHandler<IStorageService, (String CustomText, Exception Exception)> ExceptionReceived = delegate { };
 
-
-
-        public void Store(Byte[] data) => DataSendingSequence = DataSendingSequence.ContinueWith(task => StoreInternal(data));
-
-        private void StoreInternal(Byte[] data)
+        public void Store(Byte[] data)
         {
             try
             {
+                CachedFileStream.Write(data, 0, data.Length);
+                if (CachedFileStream.Length > CachedFileStreamLength)
+                {
+                    var cachedFileStream = CachedFileStream;
+                    DataFlushingSequence = DataFlushingSequence.ContinueWith(task => FlushAsync(cachedFileStream)).Unwrap();
+                    CachedFileStream = new MemoryStream(CachedFileStreamLength);
+                }
+            }
+            catch (Exception exception)
+            {
+                App.Diagnostics.Debug.Log($"{nameof(FileStorageService)}.{nameof(Store)}", exception);
+            }
+        }
+
+        private Task FlushAsync(MemoryStream stream) => Task.Run(() =>
+        {
+            try
+            {
+                var data = new Byte[stream.Length];
+                stream.Position = 0;
+                stream.Read(data, 0, data.Length);
+                stream.Dispose();
+
                 var now = DateTime.Now;
                 var fileName = $"{now.ToString("yyyy-MM-dd_HH")}";
                 if (ActiveFileName != fileName)
@@ -38,8 +60,8 @@ namespace HomeControl.Surveillance.Server.Data.File
             }
             catch (Exception exception)
             {
-                ExceptionReceived(this, ($"{nameof(FileStorageService)}.{nameof(StoreInternal)}", exception));
+                ExceptionReceived(this, ($"{nameof(FileStorageService)}.{nameof(FlushAsync)}", exception));
             }
-        }
+        });
     }
 }
