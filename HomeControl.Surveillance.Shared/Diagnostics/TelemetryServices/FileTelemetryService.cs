1a84be7ebe8b6d2aec4ef6f647d88d3fdb33793f
Venzz <venexzz@gmail.com>
2018-09-19 21:30:05 +0300

    Added missing file telemetry service.

---
+++ HomeControl.Surveillance.Shared/Diagnostics/TelemetryServices/FileTelemetryService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Venz.Telemetry
+{
+    public sealed class FileTelemetryService: ITelemetryService
+    {
+        private FileStream File;
+
+        public FileTelemetryService() { }
+
+        public void Start() => Write($"{GetTimestamp()} >> Application Launched");
+
+        public void Finish() => Write($"{GetTimestamp()} >> Application Exit");
+
+        public void LogEvent(String title) => Write($"{GetTimestamp()} >> {title}");
+
+        public void LogEvent(String title, String parameter, String value) => Write($"{GetTimestamp()} >> {title} || {parameter}: {value}");
+
+        public void LogException(String comment, Exception exception) => Write($"{GetTimestamp()} >> {comment} || {exception.GetType().FullName}: {exception.Message}");
+
+        private String GetTimestamp() => DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+        private void Write(String value)
+        {
+            try
+            {
+                var now = DateTime.Now;
+                var fileName = $"{now.ToString("yyyy-MM-dd")}.log";
+                if (Path.GetFileName(File?.Name) != fileName)
+                {
+                    File?.Flush();
+                    File?.Dispose();
+                    File = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                }
+
+                var writer = new StreamWriter(File);
+                writer.WriteLine(value);
+                writer.Flush();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
