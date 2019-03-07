60cba0bb7405f0dec702eb5d10fcd666e599e8ed
Venzz <venexzz@gmail.com>
2019-03-07 20:53:28 +0300

    Updated motion detection libraries.

--- HomeControl.Surveillance.Server/Source/Libraries/FFmpeg/Libraries.cs
+++ HomeControl.Surveillance.Server/Source/Libraries/FFmpeg/Libraries.cs
@@ -4,9 +4,16 @@ namespace FFmpeg
 {
     public static class Libraries
     {
-        public const String AvCodec = "I:\\ffmpeg-dotnet-master\\avcodec-57.dll";
-        public const String AvFormat = "I:\\ffmpeg-dotnet-master\\avformat-57.dll";
-        public const String AvUtil = "I:\\ffmpeg-dotnet-master\\avutil-55.dll";
-        public const String SwScale = "I:\\ffmpeg-dotnet-master\\swscale-4.dll";
+        #if DEBUG
+        public const String AvCodec = "Resources\\avcodec-57.dll";
+        public const String AvFormat = "Resources\\avformat-57.dll";
+        public const String AvUtil = "Resources\\avutil-55.dll";
+        public const String SwScale = "Resources\\swscale-4.dll";
+        #else
+        public const String AvCodec = "/usr/lib/arm-linux-gnueabihf/libavcodec.so.57";
+        public const String AvFormat = "/usr/lib/arm-linux-gnueabihf/libavformat.so.57";
+        public const String AvUtil = "/usr/lib/arm-linux-gnueabihf/libavutil.so.55";
+        public const String SwScale = "/usr/lib/arm-linux-gnueabihf/libswscale.so.4";
+        #endif
     }
 }
