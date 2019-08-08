2d8a198f2e4d61a50bbcbf0bd34c1cbdd2dd3577
Venzz <venexzz@gmail.com>
2019-08-08 06:52:56 +0300

    Added orient protocol header as a debug.

--- HomeControl.Surveillance.Server/Source/Data/CameraConnection/OrientProtocol/Messages/Message.cs
+++ HomeControl.Surveillance.Server/Source/Data/CameraConnection/OrientProtocol/Messages/Message.cs
@@ -6,6 +6,7 @@ namespace HomeControl.Surveillance.Server.Data.OrientProtocol
     public class Message
     {
         public UInt32 SessionId { get; private set; }
+        public UInt32 SequenceNumber { get; private set; }
         public String Header { get; private set; }
 
 
@@ -21,7 +22,8 @@ namespace HomeControl.Surveillance.Server.Data.OrientProtocol
             {
                 var marker = reader.ReadInt32();
                 var sessionId = reader.ReadUInt32();
-                reader.ReadBytes(6);
+                var sequenceNumber = reader.ReadUInt32();
+                reader.ReadBytes(2);
                 var operationCode = reader.ReadUInt16();
                 var messageDataSize = reader.ReadInt32();
                 var messageData = reader.ReadBytes(messageDataSize);
@@ -29,13 +31,13 @@ namespace HomeControl.Surveillance.Server.Data.OrientProtocol
                 switch (operationCode)
                 {
                     case (UInt16)Operation.AuthorizationResponse:
-                        return new AuthorizationResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20) };
+                        return new AuthorizationResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20), SequenceNumber = sequenceNumber };
                     case (UInt16)Operation.MediaDataResponse:
-                        return new MediaDataResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20) };
+                        return new MediaDataResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20), SequenceNumber = sequenceNumber };
                     case (UInt16)Operation.OpMonitorClaimResponse:
-                        return new OpMonitorClaimResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20) };
+                        return new OpMonitorClaimResponseMessage(sessionId, messageData) { Header = data.ToTestView(0, 20), SequenceNumber = sequenceNumber };
                     default:
-                        return new UnknownResponseMessage(sessionId, data) { Header = data.ToTestView(0, 20) };
+                        return new UnknownResponseMessage(sessionId, data) { Header = data.ToTestView(0, 20), SequenceNumber = sequenceNumber };
                 }
             }
         }
