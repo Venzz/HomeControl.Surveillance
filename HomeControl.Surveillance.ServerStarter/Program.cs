eb4ef9e5ec980c0f75beff360f3534981e38fb0b
Venzz <venexzz@gmail.com>
2020-11-18 21:26:05 +0300

    Added server auto-launcher project.

---
+++ HomeControl.Surveillance.ServerStarter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace HomeControl.Surveillance.ServerStarter
+{
+    public class Program
+    {
+        private const String ServerApplication = "HomeControl/HomeControl.Surveillance.Server.dll";
+
+        public static void Main(String[] args)
+        {
+            var process = Process.Start("dotnet", ServerApplication);
+            while (true)
+            {
+                while (!process.HasExited)
+                    Task.Delay(1000).Wait();
+
+                process = Process.Start("dotnet", ServerApplication);
+            }
+        }
+    }
+}
