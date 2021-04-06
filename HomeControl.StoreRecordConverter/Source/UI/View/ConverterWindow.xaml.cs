fd48791aa9c8f9625a6d94e79d9554bf6b4c933f
Venzz <venexzz@gmail.com>
2021-04-06 20:54:22 +0300

    Added Store Record Converter project.

---
+++ HomeControl.StoreRecordConverter/Source/UI/View/ConverterWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace HomeControl.StoreRecordConverter
+{
+    public partial class ConverterWindow: Window
+    {
+        public ConverterWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
