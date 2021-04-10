098f46aa7bba80f98f4e40e2bdfbc2c624d58758
Venzz <venexzz@gmail.com>
2021-04-10 09:20:50 +0300

    Added UI for the file converter.

--- HomeControl.StoreRecordConverter/App.xaml.cs
+++ HomeControl.StoreRecordConverter/App.xaml.cs
@@ -1,8 +1,17 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
 
 namespace HomeControl.StoreRecordConverter
 {
     public partial class App: Application
     {
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            base.OnStartup(args);
+            var executablePath = Assembly.GetExecutingAssembly().Location;
+            var workingDirectory = Path.GetDirectoryName(executablePath);
+            Directory.SetCurrentDirectory(workingDirectory);
+        }
     }
 }
--- HomeControl.StoreRecordConverter/Source/UI/View/ConverterWindow.xaml.cs
+++ HomeControl.StoreRecordConverter/Source/UI/View/ConverterWindow.xaml.cs
@@ -1,21 +1,18 @@
-﻿using HomeControl.Surveillance.Data.Storage;
+﻿using HomeControl.StoreRecordConverter.Controller;
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
+using System.Linq;
 using System.Windows;
 
-namespace HomeControl.StoreRecordConverter
+namespace HomeControl.StoreRecordConverter.View
 {
     public partial class ConverterWindow: Window
     {
+        private ConverterController Context = new ConverterController();
+
         public ConverterWindow()
         {
             InitializeComponent();
-            var executablePath = Assembly.GetExecutingAssembly().Location;
-            var workingDirectory = Path.GetDirectoryName(executablePath);
-            Directory.SetCurrentDirectory(workingDirectory);
+            DataContext = Context;
         }
 
         private async void OnDrop(Object sender, DragEventArgs args)
@@ -24,41 +21,9 @@ namespace HomeControl.StoreRecordConverter
                 return;
 
             var files = (String[])args.Data.GetData(DataFormats.FileDrop);
-            await Task.Run(() =>
-            {
-                foreach (var file in files)
-                {
-                    using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read))
-                    using (var fileReader = new BinaryReader(fileStream))
-                    {
-                        var mediaDescriptors = StoredRecordFile.ReadMediaDescriptors(fileStream);
-                        if (mediaDescriptors.Count == 0)
-                            continue;
-
-                        var tempFile = new FileInfo("temp.h264");
-                        using (var tempFileWriter = new BinaryWriter(tempFile.OpenWrite()))
-                        {
-                            foreach (var mediaDescriptor in mediaDescriptors)
-                            {
-                                fileStream.Seek(mediaDescriptor.Offset, SeekOrigin.Begin);
-                                var size = fileReader.ReadInt32();
-                                var data = fileReader.ReadBytes(size);
-                                tempFileWriter.Write(data);
-                            }
-                        }
-
-                        var mp4File = $"{Path.GetDirectoryName(file)}\\{Path.GetFileNameWithoutExtension(file)}.mp4";
-                        var process = new Process();
-                        process.StartInfo.FileName = $"FFmpeg\\ffmpeg.exe";
-                        process.StartInfo.Arguments = $"-y -i \"{tempFile.FullName}\" -c:v copy -f mp4 \"{mp4File}\"";
-                        process.Start();
-                        process.WaitForExit();
-                        process.Close();
-
-                        tempFile.Delete();
-                    }
-                }
-            });
+            Context.Add(files.ToList());
+            foreach (var file in Context.Files)
+                await Context.ConvertAsync(file);
         }
     }
 }
