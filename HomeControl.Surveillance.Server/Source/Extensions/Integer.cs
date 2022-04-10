ce9a1ed044d00456ca0dc4009a7303840c4dceac
Venzz <venexzz@gmail.com>
2022-04-10 21:58:43 +0300

    Updated file storage implementation and added auto file removal when there is not enough space left.

---
+++ HomeControl.Surveillance.Server/Source/Extensions/Integer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeControl.Surveillance
+{
+    public static class Integer
+    {
+        public static String ToDataLength(this Byte dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this Int16 dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this UInt16 dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this Int32 dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this UInt32 dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this Int64 dataLength) => ToDataLengthInternal(dataLength);
+
+        public static String ToDataLength(this UInt64 dataLength) => ToDataLengthInternal((Int64)dataLength);
+
+        public static String ToDataLengthInternal(Int64 dataLength)
+        {
+            if (dataLength >= 1024 * 1024 * 1024)
+                return $"{((Double)dataLength / 1024 / 1024 / 1024):0.00} gb";
+            else if (dataLength >= 1024 * 1024)
+                return $"{((Double)dataLength / 1024 / 1024):0.00} mb";
+            else if (dataLength >= 1024)
+                return $"{((Double)dataLength / 1024):0.00} kb";
+            else
+                return $"{dataLength:0.00} b";
+        }
+    }
+}
--- HomeControl.Surveillance.Server/Source/Services/Storage/File/FileStorageService.cs
+++ HomeControl.Surveillance.Server/Source/Services/Storage/File/FileStorageService.cs
@@ -9,31 +9,43 @@ namespace HomeControl.Surveillance.Server.Services
 {
     public class FileStorageService: IStorageService
     {
-        private TimeSpan CachedDurationLength = TimeSpan.FromMinutes(10);
-        private DateTime CachedDataDate = DateTime.Now;
-        private List<IMediaData> CachedData = new List<IMediaData>();
-        private Task DataFlushingSequence = Task.CompletedTask;
-        private Stream ActiveFileStream;
-        private String ActiveFileName;
+        private TimeSpan DataCacheDuration = TimeSpan.FromMinutes(10);
+        private List<(DateTime DateAdded, IMediaData Data)> DataCache = new List<(DateTime, IMediaData)>();
+        private Task DataStoringSequence = Task.CompletedTask;
+        private DriveInfo Drive;
+        private DirectoryInfo CurrentDirectory;
 
         public event TypedEventHandler<IStorageService, (String CustomText, String Parameter)> LogReceived = delegate { };
         public event TypedEventHandler<IStorageService, (String CustomText, Exception Exception)> ExceptionReceived = delegate { };
 
 
 
+        public FileStorageService()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            CurrentDirectory = new DirectoryInfo(currentDirectory);
+            foreach (var drive in DriveInfo.GetDrives())
+                if (currentDirectory.StartsWith(drive.Name))
+                    Drive = drive;
+
+            if (Drive == null)
+                throw new InvalidOperationException("Drive not found.");
+        }
+
         public void Store(IMediaData mediaData)
         {
             try
             {
-                CachedData.Add(mediaData);
-                var now = DateTime.Now;
-                if ((now - CachedDataDate > CachedDurationLength) || (now.Hour != CachedDataDate.Hour))
+                lock (this)
                 {
-                    var cachedData = CachedData;
-                    var cachedDataDate = CachedDataDate;
-                    DataFlushingSequence = DataFlushingSequence.ContinueWith(task => FlushAsync(cachedData, cachedDataDate)).Unwrap();
-                    CachedData = new List<IMediaData>();
-                    CachedDataDate = now;
+                    var now = DateTime.Now;
+                    DataCache.Add((now, mediaData));
+                    if (now - DataCache[0].DateAdded > DataCacheDuration)
+                    {
+                        var dataCache = DataCache;
+                        DataStoringSequence = DataStoringSequence.ContinueWith(task => StoreCache(dataCache));
+                        DataCache = new List<(DateTime, IMediaData)>();
+                    }
                 }
             }
             catch (Exception exception)
@@ -42,44 +54,53 @@ namespace HomeControl.Surveillance.Server.Services
             }
         }
 
-        private Task FlushAsync(List<IMediaData> mediaData, DateTime mediaDataDate) => Task.Run(() =>
+        private void StoreCache(List<(DateTime DateAdded, IMediaData Data)> dataCache)
         {
             try
             {
-                var mediaStream = new MemoryStream();
-                var mediaDataItems = mediaData.Select(a => (new StoredRecordFile.MediaDataDescriptor() { Type = a.MediaDataType, Timestamp = a.Timestamp, Duration = a.Duration }, a.Data)).ToList();
-                var storedRecord = new StoredRecordFile(mediaStream);
-                storedRecord.WriteSlice(mediaDataItems);
-
-                var fileName = $"{mediaDataDate.ToString("yyyy-MM-dd_HH")}";
-                if (ActiveFileName != fileName)
+                if (Drive.AvailableFreeSpace / 1024 / 1024 / 1024 < 1)
                 {
-                    if (ActiveFileStream != null)
+                    var files = new List<FileInfo>();
+                    foreach (var file in CurrentDirectory.GetFiles("*.*", SearchOption.TopDirectoryOnly).OrderBy(a => a.CreationTime).Where(a => a.Extension == ".sr" || a.Extension == ".log"))
+                        files.Add(file);
+
+                    while (Drive.AvailableFreeSpace / 1024 / 1024 / 1024 < 2 && files.Count > 0)
                     {
-                        ActiveFileStream.Flush();
-                        ActiveFileStream.Close();
+                        LogReceived(this, (nameof(FileStorageService), $"Deleted {files[0].Name}."));
+                        files[0].Delete();
+                        files.RemoveAt(0);
                     }
-                    ActiveFileStream = new FileStream($"{fileName}.sr", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
-                    ActiveFileName = fileName;
                 }
 
-                mediaStream.Seek(0, SeekOrigin.Begin);
-                mediaStream.CopyTo(ActiveFileStream);
-                mediaStream.Dispose();
-                ActiveFileStream.Flush();
-                LogReceived(this, (nameof(FileStorageService), $"Stored {mediaStream.Length} of data."));
+                var dataCacheByHour = dataCache.GroupBy(a => a.DateAdded.Hour);
+                foreach (var dataCacheSlice in dataCacheByHour)
+                {
+                    var mediaStream = new MemoryStream();
+                    var mediaDataItems = dataCacheSlice.Select(a => (new StoredRecordFile.MediaDataDescriptor() { Type = a.Data.MediaDataType, Timestamp = a.Data.Timestamp, Duration = a.Data.Duration }, a.Data.Data)).ToList();
+                    var storedRecord = new StoredRecordFile(mediaStream);
+                    storedRecord.WriteSlice(mediaDataItems);
+                    var mediaStreamLength = mediaStream.Length;
+
+                    var fileName = $"{dataCacheSlice.First().DateAdded.ToString("yyyy-MM-dd_HH")}.sr";
+                    using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    {
+                        mediaStream.Seek(0, SeekOrigin.Begin);
+                        mediaStream.CopyTo(fileStream);
+                        mediaStream.Dispose();
+                        LogReceived(this, (nameof(FileStorageService), $"Stored {mediaStreamLength.ToDataLength()} to {fileName}."));
+                    }
+                }
             }
             catch (Exception exception)
             {
-                ExceptionReceived(this, ($"{nameof(FileStorageService)}.{nameof(FlushAsync)}", exception));
+                ExceptionReceived(this, ($"{nameof(FileStorageService)}.{nameof(StoreCache)}", exception));
             }
-        });
+        }
 
         public IReadOnlyCollection<String> GetStoredRecords()
         {
             var storedRecords = new List<String>();
-            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
-            foreach (var file in directory.GetFiles("*.sr", SearchOption.TopDirectoryOnly))
+            foreach (var file in CurrentDirectory.GetFiles("*.sr", SearchOption.TopDirectoryOnly))
                 storedRecords.Add(file.Name);
             return storedRecords;
         }
